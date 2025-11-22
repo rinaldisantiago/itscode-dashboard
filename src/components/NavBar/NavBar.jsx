f1164e693d51981c5018a442e6e5bc6437450d7f
almironemiliano <almironemiliano@hola.com>
2025-11-21 23:47:50 -0300

    conectando con Users del back

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,12 +10,12 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link to="/users">User</Link></Nav.Link>
-            <Nav.Link><Link to="/admin">Admin</Link></Nav.Link>
+            <Nav.Link><Link to={"/users"}>User</Link></Nav.Link>
+            <Nav.Link><Link to={"/admin"}>Admin</Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,63 @@
-import React from "react";
+import React, { use } from "react";
+import Paginado from './components/Paginador/Paginador';
+import Button from '../Button/Button';
+import Label from '../Label/Label';
 
 function Users() {
+    const [query, setQuery] = useState("");
+    const [pageNumber, setPageNumber] = useState(1);
+    const [users, setUsers] = useState([]);
+
+
+    const anterior = () => {
+        let numero = pageNumber;
+        if (numero > 1) {
+            numero--;
+            setPageNumber(numero);
+        }
+    }
+
+    const siguiente = () => {
+        let numero = pageNumber;
+        if (numero < 10) {
+            numero++;
+            setPageNumber(numero);
+        }
+    }
+
+    return (
+        <>
+            <Button text="Anterior" callback={anterior}/>
+            <Label text={pageNumber}/>
+            <Button text="Siguiente" callback={siguiente}/>
+        </>
+    )
+
+
+    const find = (evt) => {
+        const {value} = evt.target;
+        setQuery(value);
+    }
+
+    const fetchData = async () => {
+        try {
+            let response = await fetch(`'http://localhost:5052/User/${pageNumber}/5'`);
+            let json = await response.json();
+
+            setUsers(json.users);
+        } catch (error) {
+            alert("Error al traer los usuarios");
+        } finally {
+            
+        }
+    }
+
+
+    useEffect(() => {
+        fetchData();
+    }, [pageNumber, query]);
+
+
     return (
         <div>
             USERS
