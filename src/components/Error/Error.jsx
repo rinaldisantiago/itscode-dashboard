4ee82376c8d416c9b3ffa35222851c8eef5f4690
Hernandez-Carlos <120617047+HernandezCarlosIsaias@users.noreply.github.com>
2025-12-02 22:21:39 -0300

    agregamos imagen error 404

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import './Error.css';
 
 function Error() {
     return (
-        <div>
-            Oh... pagina no encontrada
+        <div className="errorContainer">
+            <h1 className="errorMessage">Tranqui, solo es un error...</h1>
+            <div className="imageConteiner">
+                <img src="/deadpool.png" alt="Error" />
+            </div>
         </div>
     )
 }
