a9fa5d28d8ea0e70a394e24d4834540a68161c86
Santiago Tomas Rinaldi <santiago.rinaldi@itscordoba.edu.ar>
2025-12-07 15:48:24 -0300

    Pequeñas soluciones aplicadas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Users from './components/Users/Users';
 import Admin from './components/Admin/Admin';
 import Login from './components/Login/Login';
 import Baneo from './components/Ban/Ban';
-import Bienvinida from './components/Bienvenida/Bienvenida';
+import Bienvenida from './components/Bienvenida/Bienvenida';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Editar from './components/Editar/Editar';
 
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route exact path="/" element= {<Bienvinida/>} />
+          <Route exact path="/" element= {<Bienvenida/>} />
           <Route exact path="/login" element= {<Login/>} />
           <Route exact path="/users" element= {<PrivateRoute><Users/></PrivateRoute>}/>
           <Route exact path="/admin" element= {<PrivateRoute><Admin/></PrivateRoute>}/>
